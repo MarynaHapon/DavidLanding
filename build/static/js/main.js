12f12a8aa633b48ac6fa888c9ef6b918a45f4686
Maryna Hapon <marynahapon@gmail.com>
2017-08-02 01:02:31 +0300

    progressbar

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,10 +1,11 @@
+    // menu
 $('.toggle').click(function() {
     $('.toggle').toggleClass('active');
     $('#overlay').toggleClass('open');
     $('#overlay-hidden').toggleClass('hidden-wrap'); // .hidden-wrap --> common_styles.styl
 });
 
-// external js: flickity.pkgd.js
+    // carousel
 var $carousel = $('.carousel').flickity({
     autoPlay: 3000,
     imagesLoaded: true,
@@ -13,11 +14,9 @@ var $carousel = $('.carousel').flickity({
 });
 
 var $imgs = $carousel.find('.carousel-cell img');
-// get transform property
 var docStyle = document.documentElement.style;
 var transformProp = typeof docStyle.transform == 'string' ?
     'transform' : 'WebkitTransform';
-// get Flickity instance
 var flkty = $carousel.data('flickity');
 
 $carousel.on( 'scroll.flickity', function() {
@@ -28,6 +27,63 @@ $carousel.on( 'scroll.flickity', function() {
     });
 });
 
+    // animations for modules
 AOS.init({
     easing: 'ease-in-out-sine'
 });
+
+    // progressbar
+var progressBar1 = new ProgressBar.Circle(infoProgressBar1, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#4cb027',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: null
+});
+progressBar1.animate(0.3125);
+
+var progressBar2 = new ProgressBar.Circle(infoProgressBar2, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#4cb027',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: null
+});
+progressBar2.animate(0.0625);
+
+var progressBar3 = new ProgressBar.Circle(infoProgressBar3, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#4cb027',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: null
+});
+progressBar3.animate(0.0625);
+
+var progressBar4 = new ProgressBar.Circle(infoProgressBar4, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#4cb027',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: null
+});
+progressBar4.animate(0.125);
+
+var progressBar5= new ProgressBar.Circle(infoProgressBar5, {
+    strokeWidth: 6,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#4cb027',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: null
+});
+progressBar5.animate(0.0625);
